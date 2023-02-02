e102c9301172501bded5399a49a8623d2515a879
polyqubit <polyumbrial@gmail.com>
2023-02-02 10:20:57 -0800

    next and react !!

---
+++ nextjs-blog/pages/postlist.js
@@ -0,0 +1,8 @@
+export default function PostList() {
+    return <div>
+        <ul>
+            <li><a href="http://localhost:3000/posts/post-1">Post 1</a></li>
+        </ul>
+        <a href="http://localhost:3000">Back</a>
+    </div>
+}
---
+++ nextjs-blog/pages/posts/post-1.js
@@ -0,0 +1,8 @@
+export default function Post() {
+    return <>
+        <h1>
+            <em>post 1</em>
+        </h1>
+        <a href="http://localhost:3000/postlist">Back</a>
+    </>;
+}
